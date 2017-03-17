dca2544422e0173cc02ada5cb356ce246ea4e86a
joshwoo70 <joshwoo69@hotmail.sg>
2017-03-17 23:56:36 +0800

    Removed Redundant code Kitten mentioned it can be removed. [ci skip]

--- src/main/java/aobd/Ores/OreDict.java
+++ src/main/java/aobd/Ores/OreDict.java
@@ -5,9 +5,7 @@ import net.minecraftforge.oredict.OreDictionary;
  * Created by joshw on 17/2/2017.
  */
 public class OreDict {
-    public String[] Graboredictlist() {
-        return OreDictionary.getOreNames();
-    }
+
     public void filteroredict() {
     }
 }
